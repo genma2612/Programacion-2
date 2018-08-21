8cc9c49e0761e08b092d48d6364c77401252a882
genma2612 <manuelagustingonzalez@gmail.com>
2018-08-21 11:07:41 -0300

    Break

---
+++ Gonzalez.Manuel.2A/Ejercicio00/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Ingresar nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es: {0}", nombre); // Las "mascaras" se muestran como {0}, segùn cant. de parametros.
+
+
+
+
+        }
+    }
+}
