6998966955300083874236873f8fac6cff4d5b86
genma2612 <manuelagustingonzalez@gmail.com>
2018-08-21 13:38:25 -0300

    Correccion int.parse

--- Gonzalez.Manuel.2A/Ejercicio00/Program.cs
+++ Gonzalez.Manuel.2A/Ejercicio00/Program.cs
@@ -16,7 +16,7 @@ namespace Ejercicio00
             Console.WriteLine("Ingresar nombre:");
             nombre = Console.ReadLine();
             Console.WriteLine("Ingresar edad:");
-            int.Parse(edad) = Console.ReadLine(); //"." para metodos y atributos
+            edad = int.Parse(Console.ReadLine()); //"." en tipo de dato "int" para metodos y atributos
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Su nombre es: {0} y su edad: {1}", nombre, edad); // Las "mascaras" se muestran como {0}, segùn cant. de parametros.
         }
