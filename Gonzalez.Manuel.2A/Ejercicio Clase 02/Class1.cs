8777e8e4196cfdf162b3777d017490be1b92e080
genma2612 <manuelagustingonzalez@gmail.com>
2018-08-28 12:03:40 -0300

    Final Clase 3

--- Gonzalez.Manuel.2A/Ejercicio Clase 02/Class1.cs
+++ Gonzalez.Manuel.2A/Ejercicio Clase 02/Class1.cs
@@ -10,19 +10,53 @@ namespace Entidades
     {
         public static string mensaje;
         public static ConsoleColor color = ConsoleColor.Red;
+
         public static string Imprimir()
         {
-            return Sello.mensaje;
+            if (Sello.TryParse(Sello.mensaje, out string cadena))
+            {
+                cadena = ArmarMensajeConFormato();
+            }
+            else
+                cadena = "El mensaje està vacio.";
+            return cadena;
         }
+
         public static void Borrar()
         {
-            Sello.mensaje = "...";
+            Sello.mensaje = "";
         }
+
         public static void ImprimirEnColor()
         {
             Console.ForegroundColor = Sello.color;
             Console.WriteLine(Sello.Imprimir());
             Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private static string ArmarMensajeConFormato()
+        {
+            int largo = Sello.mensaje.Length;
+            string mensajeFormateado;
+            string asteriscos = ""; //Inicializar la string que serà acumulada
+            for (int i = 0; i < largo+2; i++)
+            {
+                asteriscos += "*";
+            }
+            mensajeFormateado = asteriscos + "\n*" + Sello.mensaje + "*\n" + asteriscos;
+            return mensajeFormateado;
+        }
+
+        private static bool TryParse(string cadena, out string CadenaParsed)
+        {
+            bool retorno = false;
+            CadenaParsed = "";
+            if (cadena.Length > 0)
+            {
+                CadenaParsed = cadena;
+                retorno = true;
+            }
+            return retorno;
         }
     }
 }
