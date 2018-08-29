a89ef8fa3787e61ce062b6b45d9efa13e1f8195f
genma2612 <manuelagustingonzalez@gmail.com>
2018-08-29 12:05:59 -0300

    Final Clase 4

---
+++ Gonzalez.Manuel.2A/Ejercicio Clase 04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Clase 04";
+
+            DateTime fecha = new DateTime(1993,09,03);
+
+            Cosa primerCosa  = new Cosa();
+            Cosa segundaCosa = new Cosa("Cadena añadida 1");
+            Cosa terceraCosa = new Cosa("Cadena añadida 2", 24);
+            Cosa cuartaCosa  = new Cosa("Cadena añadida 3", 24, fecha);
+
+            Console.WriteLine(Cosa.Mostrar(primerCosa));
+            Console.WriteLine(Cosa.Mostrar(segundaCosa));
+            Console.WriteLine(Cosa.Mostrar(terceraCosa));
+            Console.WriteLine(Cosa.Mostrar(cuartaCosa));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gonzalez.Manuel.2A/Entidades/Cosa.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Cosa
+    {
+        #region Atributos
+
+        public string cadena;
+        public double numero;
+        public DateTime fecha;
+
+        #endregion
+
+        #region Mètodos
+
+        public static string Mostrar(Cosa elemento)
+        {
+            return elemento.Mostrar();
+        }
+
+        private string Mostrar()
+        {
+            return this.cadena + " " + this.numero.ToString() + " " + this.fecha.ToShortDateString();
+        }
+
+        public void EstablecerValor(string c)
+        {
+            this.cadena = c;
+        }
+
+        public void EstablecerValor(double n)
+        {
+            this.numero = n;
+        }
+
+        public void EstablecerValor(DateTime f)
+        {
+            this.fecha = f;
+        }
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor por defecto con valores inicializados.
+        /// </summary>
+        public Cosa()
+        {
+            this.cadena = "Sin valor";
+            this.numero = 1.9;
+            this.fecha = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor con ingreso de cadena.
+        /// </summary>
+        /// <param name="c">Cadena a ingresar.</param>
+        public Cosa(string c) : this() //invoco al constructor por defecto y luego cambio el valor de la cadena.
+        {
+            this.cadena = c;
+            //this.numero = 1.9;
+            //this.fecha = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor con ingreso de cadena y numero
+        /// </summary>
+        /// <param name="c">Cadena a ingresar.</param>
+        /// <param name="n">Numero a ingresar.</param>
+        public Cosa(string c, double n) :this(c)
+        {
+            //this.cadena = "Sin valor";
+            this.numero = n;
+            //this.fecha = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor con ingreso de cadena, numero y fecha
+        /// </summary>
+        /// <param name="c">Cadena a ingresar.</param>
+        /// <param name="n">Numero a ingresar.</param>
+        /// <param name="f">Fecha a ingresar.</param>
+        public Cosa(string c, double n, DateTime f) :this(c,n)
+        {
+            //this.cadena = "Sin valor";
+            //this.numero = 1.9;
+            this.fecha = f;
+        }
+
+        #endregion
+    }
+}
