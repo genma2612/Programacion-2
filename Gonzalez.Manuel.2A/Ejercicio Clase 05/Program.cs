cb78ab58011f987fe47f5236aa85d5baf3c1c451
genma2612 <manuelagustigonzalez@gmail.com>
2018-09-04 12:04:50 -0300

    Final Clase 05

---
+++ Gonzalez.Manuel.2A/Ejercicio Clase 05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Clase 05";
+
+
+            ConsoleColor red = ConsoleColor.Red;
+            ETipoTinta tipo = ETipoTinta.China;
+            Tinta T1 = new Tinta();
+            Tinta T2 = new Tinta(red);
+            Tinta T3 = new Tinta(red, tipo);
+            Tinta T4 = new Tinta();
+
+            Console.WriteLine(Tinta.Mostrar(T1));
+            Console.WriteLine(Tinta.Mostrar(T2));
+            Console.WriteLine(Tinta.Mostrar(T3));
+            if (T1 == T4)
+                Console.WriteLine(Tinta.Mostrar(T4));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Gonzalez.Manuel.2A/Ejercicio Clase 05/Tinta.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_05
+{
+    class Tinta
+    {
+        private ConsoleColor _color;
+        private ETipoTinta _tipo;
+
+        #region Mètodos
+
+        public static string Mostrar(Tinta elemento)
+        {
+            return elemento.Mostrar();
+        }
+
+        private string Mostrar()
+        {
+            string retorno;
+            retorno = "La tinta es " + this._tipo + " de color " + this._color + ".";
+            return retorno;
+        }
+
+        #region Sobrecarga de operador "=="
+
+        public static bool operator ==(Tinta t1,Tinta t2)
+        {
+            return t1._color == t2._color && t1._tipo == t2._tipo;
+        }
+
+        public static bool operator !=(Tinta t1, Tinta t2)
+        {
+            return !(t1 == t2);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constuctor
+
+        public Tinta()
+        {
+            this._color = ConsoleColor.Blue;
+            this._tipo = ETipoTinta.ConBrillito;
+        }
+
+        public Tinta(ConsoleColor color) : this()
+        {
+            this._color =color;
+        }
+
+        public Tinta(ConsoleColor color, ETipoTinta tipo) : this(color)
+        {
+            this._tipo = tipo;
+        }
+
+        #endregion
+    }
+}
