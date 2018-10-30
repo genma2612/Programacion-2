7b695ee9c7f60596687e7ae23c5e40e7165eb1a7
genma2612 <manuelagustingonzalez@gmail.com>
2018-10-30 10:15:07 -0300

    Final Clase 17

---
+++ Gonzalez.Manuel.2A.Clase/Entidades.Clase17.test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Entidades.Clase17;
+
+namespace Entidades.Clase17.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AdministradorArchivos.Escribir(@"C:\Users\alumno\Desktop\Escritorio.txt", "Escritorio"); //Clase Enviorenment para rutas predefinidas
+            AdministradorArchivos.Escribir(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Escritorio.txt" , "Escritorio");
+            if (AdministradorArchivos.Leer(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Escritorio.txt", out string e))
+                Console.WriteLine(e);
+            AdministradorArchivos.Escribir(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Documentos.txt", "Documentos");
+            if (AdministradorArchivos.Leer(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Documentos.txt", out string d))
+                Console.WriteLine(d);
+            AdministradorArchivos.Escribir(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\Imagenes.txt", "Imagenes");
+            if(AdministradorArchivos.Leer(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\Imagenes.txt", out string i))
+                Console.WriteLine(i);
+            AdministradorArchivos.Escribir(AppDomain.CurrentDomain.BaseDirectory + "\\Base.txt", "Base"); //Clase AppDomain ruta del .exe
+            if (AdministradorArchivos.Leer(AppDomain.CurrentDomain.BaseDirectory + "\\Base.txt", out string b))
+                Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gonzalez.Manuel.2A.Clase/Entidades.Clase17/AdministradorArchivos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Entidades.Clase17
+{
+    public static class AdministradorArchivos
+    {
+        public static bool Escribir(string path, string cadena)
+        {
+            bool retorno = false;
+            try
+            {
+                StreamWriter sw;
+                using (sw = new StreamWriter(path, false))
+                {
+                    sw.Write(cadena);
+                    retorno = true;
+                }
+            }
+            catch (Exception c)
+            {
+            }
+            return retorno;
+        }
+
+        public static bool Leer(string path, out string cadena)
+        {
+            bool retorno = false;
+            cadena = "";
+            try
+            {
+                StreamReader sr;
+                using (sr = new StreamReader(path, false))
+                {
+                    cadena = sr.ReadToEnd();
+                    retorno = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return retorno;
+        }
+    }
+}
